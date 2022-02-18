b14813eded95201ca94c5ff632fe1fe6f0dbb039
Ivan Kazakov <ivankaz9@yandex.ru>
2022-02-19 00:53:14 +0400

    Добавил контроллер и представления для админки

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,41 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+  <div class="container">
+    <div class="row lead">
+      <div class="col-sm-3">
+        Категорий <span class="text-primary">0</span>
+      </div>
+      <div class="col-sm-3">
+        Материалов <span class="text-danger">0</span>
+      </div>
+      <div class="col-sm-3">
+        Посетителей за все время <span class="text-warning">0</span>
+      </div>
+      <div class="col-sm-3">
+        Посетителей за сегодня <span class="text-info">0</span>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-6 d-grid gap-2">
+        <a class="btn btn-info" href="#">Создать категорию</a>
+        <a class="list-group-item" href="#">
+          <h4 class="list-group-item-heading">Категория первая</h4>
+          <p class="list-group-item-text">
+            Кол-во материалов
+          </p>
+        </a>
+      </div>
+      <div class="col-sm-6 d-grid gap-2">
+        <a class="btn btn-info" href="#">Создать материал</a>
+        <a class="list-group-item" href="#">
+          <h4 class="list-group-item-heading">Материал первый</h4>
+          <p class="list-group-item-text">
+            Категория
+          </p>
+        </a>
+      </div>
+    </div>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group([
+  'prefix' => 'admin',
+  'namespace' => 'Admin',
+  'middleware' => ['auth']
+], function () {
+   Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'dashboard'])->name('admin.index');
+});
