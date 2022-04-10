42d6eff37ffa7633262911406602503119079e6a
Ivan Kazakov <ivankaz9@yandex.ru>
2022-04-10 20:20:39 +0400

    Добавил ссылку на панель состояния в меню админки

--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -34,6 +34,9 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                       <li class="nav-item">
+                        <a class="nav-link" href="{{route('admin.index')}}">Панель состояния</a>
+                      </li>
+                      <li class="nav-item">
                         <a class="nav-link" href="{{route('admin.category.index')}}">Категории</a>
                       </li>
                       <li class="nav-item">
