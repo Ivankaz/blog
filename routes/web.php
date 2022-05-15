6f7e86b49f7f7ed62b7e60d7c0251db781b7cab7
Ivan Kazakov <ivankaz9@yandex.ru>
2022-05-15 12:26:14 +0400

    Исправил путь в админку, на который перенаправляется пользователь после успешной авторизации

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::group([
   'prefix' => 'admin',
   'namespace' => 'App\Http\Controllers\Admin',
