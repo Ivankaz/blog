f28a882b86ff5dba96d1689dc1be7d53339a3d8f
Ivan Kazakov <ivankaz9@yandex.ru>
2022-06-07 17:47:57 +0400

    Сделал удаление пользователей в админке

--- app/Http/Controllers/Admin/UserManagement/UserController.php
+++ app/Http/Controllers/Admin/UserManagement/UserController.php
@@ -117,6 +117,8 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect()->route('admin.user_management.user.index');
     }
 }
